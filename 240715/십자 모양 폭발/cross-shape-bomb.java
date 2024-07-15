2d11b79e82017d915ba627b3596deec0c0e7a034
dmdkr55 <59314089+dmdkr55@users.noreply.github.com>
2024-07-15 21:50:59 +0900

    Title: 십자 모양 폭발, Time: 121ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240715/십자 모양 폭발/cross-shape-bomb.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+
+public class Main {
+
+    static int n;
+    static int[][] arr;
+    static int r, c;
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        arr = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        r = sc.nextInt();
+        c = sc.nextInt();
+        r -= 1;
+        c -= 1;
+
+        // 폭탄 터트리기
+        int selectedValue = arr[r][c];
+        arr[r][c] = 0;
+        for (int i = 1; i < selectedValue; i++) {
+            bomb(i);
+        }
+
+        // 빈칸 밀기
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (arr[i][j] == 0) {
+                    arr[i][j] = arr[i - 1][j];
+                    arr[i - 1][j] = 0;
+                }
+            }
+        }
+
+        // 출력
+        for (int[] a : arr) {
+            for (int v : a) {
+                System.out.print(v + " ");
+            }
+            System.out.println();
+        }
+
+    }
+
+    private static void bomb(int v) {
+        if (r - v >= 0) {
+            arr[r - v][c] = 0;
+        }
+        if (r + v < n) {
+            arr[r + v][c] = 0;
+        }
+        if (c - v >= 0) {
+            arr[r][c - v] = 0;
+        }
+        if (c + v < n) {
+            arr[r][c + v] = 0;
+        }
+    }
+}
