23da126ed83eaa1fa791c848750e0ce6c643ff57
dmdkr55 <59314089+dmdkr55@users.noreply.github.com>
2024-07-15 20:50:47 +0900

    Title: 삼각형 컨베이어 벨트, Time: 234ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240715/삼각형 컨베이어 벨트/conveyor-belt-triangle.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Main {
+
+    static int n;
+    static int t;
+    static int[][] arr;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        t = sc.nextInt();
+        arr = new int[3][n];
+        for (int i = 0; i < n; i++) {
+            arr[0][i] = sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            arr[1][i] = sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            arr[2][i] = sc.nextInt();
+        }
+
+        while (t > 0) {
+            int temp0 = arr[0][n - 1];
+            for (int i = n - 1; i > 0; i--) {
+                arr[0][i] = arr[0][i - 1];
+            }
+            int temp1 = arr[1][n - 1];
+            for (int i = n - 1; i > 0; i--) {
+                arr[1][i] = arr[1][i - 1];
+            }
+            int temp2 = arr[2][n - 1];
+            for (int i = n - 1; i > 0; i--) {
+                arr[2][i] = arr[2][i - 1];
+            }
+            arr[0][0] = temp2;
+            arr[1][0] = temp0;
+            arr[2][0] = temp1;
+
+            t--;
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[0][i] + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[1][i] + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[2][i] + " ");
+        }
+    }
+}
