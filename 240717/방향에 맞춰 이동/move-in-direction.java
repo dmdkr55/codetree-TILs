30af4fa48fc36caeb8f48c9e8267c2f0961fae5e
dmdkr55 <59314089+dmdkr55@users.noreply.github.com>
2024-07-17 18:53:35 +0900

    Title: 방향에 맞춰 이동, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

--- 240717/방향에 맞춰 이동/move-in-direction.java
+++ 240717/방향에 맞춰 이동/move-in-direction.java
@@ -5,24 +5,24 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+
         int n = sc.nextInt();
         int x = 0, y = 0;
-        Map<String, Integer> dx = new HashMap<>();
-        Map<String, Integer> dy = new HashMap<>();
-        dx.put("N", 0);
-        dx.put("E", 1);
-        dx.put("S", 0);
-        dx.put("W", -1);
-        dy.put("N", 1);
-        dy.put("E", 0);
-        dy.put("S", -1);
-        dy.put("W", 0);
+        int[] dx = new int[]{0, 1, 0, -1}; //N, E, S, W
+        int[] dy = new int[]{1, 0, -1, 0}; //N, E, S, W
+        int dir = 0;
+
         for (int i = 0; i < n; i++) {
             String d = sc.next();
             int a = sc.nextInt();
-            x += a * dx.get(d);
-            y += a * dy.get(d);
+            
+            if (d.charAt(0) == 'N') dir = 0;
+            else if (d.charAt(0) == 'E') dir = 1;
+            else if (d.charAt(0) == 'S') dir = 2;
+            else if (d.charAt(0) == 'W') dir = 3;
+
+            x += a * dx[dir];
+            y += a * dy[dir];
         }
 
         System.out.println(x + " " + y);
