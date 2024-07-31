51ec2ba5949ef29c8cb730eb0749b9779db8376a
dmdkr55 <59314089+dmdkr55@users.noreply.github.com>
2024-07-31 15:50:31 +0900

    Title: 피보나치 수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240731/피보나치 수/fibonacci-number.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Main {
+
+    static int n;
+    static int[] dp;
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 1;
+
+        int result = 0;
+        if (n <= 2) {
+            result = dp[n];
+        } else {
+            result = fibo(n);
+        }
+
+        System.out.println(result);
+    }
+
+    static int fibo(int n) {
+        if (dp[n] != 0)
+            return dp[n];
+        return fibo(n - 1) + fibo(n - 2);
+    }
+
+}
